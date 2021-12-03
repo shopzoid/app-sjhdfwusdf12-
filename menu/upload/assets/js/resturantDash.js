73b233470d601f9a82335b89557cd63ce45c9329
shopzoid <shopzoid.id@gmail.com>
2021-12-03 16:21:29 +0700

    first commit

--- menu/upload/assets/js/resturantDash.js
+++ menu/upload/assets/js/resturantDash.js
@@ -49,7 +49,7 @@ const showItem = () => {
                         <div class="dropdown">
                                 <i class="bi bi-three-dots-vertical dropbtn three-dot"></i>
                             <div class="dropdown-content">
-                                <a href="#" onclick="editItem(${doc.id})">Edit</a>
+                                <a onclick="editItem(${doc.id})">Edit</a>
                                 <a onclick="deleteItem(${doc.id}, '${doc.data().imagename}')" style="color: white;">Delete</a>
                             </div>
                         </div>
